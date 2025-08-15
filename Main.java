0450f3588b71886de7c0f8c6cc8dcdb3884d35e3
nataliakocol <43788637+nataliakocol@users.noreply.github.com>
2025-08-15 23:12:06 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,135 @@
+import java.util.ArrayList;
+
+class Company
+{
+    private String name;
+}
+
+class Depot
+{
+    private String name;
+    private ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+
+    public Depot(String name)
+    {
+        this.name = name;
+    }
+
+    public void addVehicle(Vehicle vehicle)
+    {
+        vehicles.add(vehicle);
+    }
+
+    public String getName()
+    {
+        return name;
+    };
+
+    public Vehicle getVehicle(String registration)
+    {
+        for(int i = 0; i < vehicles.size(); i++){
+            if(vehicles.get(i).getRegistration().equals(registration))
+                return vehicles.get(i);
+        }
+        return null;
+    };
+}
+
+abstract class Vehicle
+{
+    private String registration;
+    private int numberSeats;
+
+    public Vehicle(String registration, int numberSeats){
+        this.registration = registration;
+        this.numberSeats = numberSeats;
+    }
+
+    public String getRegistration(){
+        return registration;
+    }
+
+    public int getNumberSeats(){
+        return numberSeats;
+    }
+
+    public abstract int getHireCost(int days);
+}
+
+class Bus extends Vehicle
+{
+    public Bus(String registration, int numberSeats)
+    {
+        super(registration,numberSeats);
+
+    }
+
+    @Override
+    public int getHireCost(int days)
+    {
+        return 10;
+    }
+}
+
+class Coach extends Vehicle{
+
+    private boolean luxury;
+
+    public Coach(String registration, int numberSeats, boolean luxury){
+        super(registration, numberSeats);
+        this.luxury = luxury;
+    }
+//    public Coach()
+//    {
+//        super("aaa", 10);
+//        this.luxury = false;
+//    }
+
+
+    @Override
+    public int getHireCost(int days)
+    {
+        return 10*numberSeats;
+    }
+
+    public boolean isLuxury(){
+        return luxury;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        //Coach c;
+        //c = new Vehicle();
+        //Vehicle v = new Coach(dsds,dsds);
+
+        Depot depot = new Depot("aaaa");
+       // var veh = new Vehicle("kr758", 48);
+
+      //  depot.addVehicle(veh);
+      //  depot.addVehicle(new Vehicle("kra538", 120));
+
+        var coach = new Coach("gh5783", 67, true);
+        depot.addVehicle(coach);
+
+        var bus = new Bus("tg54", 34);
+        depot.addVehicle(bus);
+
+        Vehicle found = depot.getVehicle("tg54");
+        found.getHireCost(1);
+
+       // System.out.println(veh == found);
+
+        if(depot.getVehicle("sd45") == null)
+            System.out.println("OK");
+       // depot.name = "aaa";
+
+
+
+    }
+}
